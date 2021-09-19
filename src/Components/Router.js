9c1491842d54579545d190053f3e9760a88ebb06
Junyoung Hong <hjunyoung10@gmail.com>
2021-09-19 23:57:46 +0900

    ✨ Add detail routers

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -9,6 +9,7 @@ import Header from './Header';
 import Home from 'Routes/Home';
 import TV from 'Routes/TV';
 import Search from 'Routes/Search';
+import Detail from 'Routes/Detail';
 
 const RouterComponent = () => {
   return (
@@ -18,6 +19,8 @@ const RouterComponent = () => {
         <Route path="/" component={Home} exact />
         <Route path="/tv" component={TV} exact />
         <Route path="/search" component={Search} exact />
+        <Route path="/movie/:id" component={Detail} exact />
+        <Route path="/tv/:id" component={Detail} exact />
         <Redirect from="*" to="/" />
       </Switch>
     </Router>
