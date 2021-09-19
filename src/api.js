b1e590da3da9dadadff0d3e258e1af16b705b674
Junyoung Hong <hjunyoung10@gmail.com>
2021-09-19 16:45:06 +0900

    ✨ Use moviedb api

---
+++ src/api.js
@@ -0,0 +1,61 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'https://api.themoviedb.org/3/',
+  params: {
+    api_key: process.env.REACT_APP_MOVIE_API,
+    language: 'en-US',
+  },
+});
+
+export const tvApi = {
+  topRated() {
+    api.get('tv/top_rated');
+  },
+  popular() {
+    api.get('tv/popular');
+  },
+  airingToday() {
+    api.get('tv/airing_today');
+  },
+  showDetail(id) {
+    api.get(`tv/${id}`, {
+      params: {
+        append_to_response: 'videos,images',
+      },
+    });
+  },
+  search(term) {
+    api.get('search/tv', {
+      params: {
+        query: term,
+      },
+    });
+  },
+};
+
+export const moviesApi = {
+  nowPlaying() {
+    api.get('movie/now_playing');
+  },
+  upcoming() {
+    api.get('movie/upcoming');
+  },
+  popular() {
+    api.get('movie/popular');
+  },
+  movieDetail(id) {
+    api.get(`movie/${id}`, {
+      params: {
+        append_to_response: 'videos,images',
+      },
+    });
+  },
+  search(term) {
+    api.get('search/movie', {
+      params: {
+        query: term,
+      },
+    });
+  },
+};
