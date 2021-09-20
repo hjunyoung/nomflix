6f2894a5a579aad64f87d68c6afdd8f6e6d67df1
Junyoung Hong <hjunyoung10@gmail.com>
2021-09-20 16:22:24 +0900

    ✨ Add PropTypes to presenters

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-const DetailPresenter = () => {
+const DetailPresenter = ({ result, error, loading }) => {
   return <h1>Detail</h1>;
+};
+
+DetailPresenter.propTypes = {
+  result: PropTypes.object,
+  error: PropTypes.string,
+  loading: PropTypes.bool.isRequired,
 };
 
 export default DetailPresenter;
--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-const SearchPresenter = () => {
+const SearchPresenter = ({
+  movieResults,
+  tvResults,
+  searchTerm,
+  error,
+  loading,
+  handleSubmit,
+}) => {
   return <h1>Search</h1>;
+};
+
+SearchPresenter.propTypes = {
+  movieResults: PropTypes.array,
+  tvResults: PropTypes.array,
+  searchTerm: PropTypes.string,
+  error: PropTypes.string,
+  loading: PropTypes.bool.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
 };
 
 export default SearchPresenter;
