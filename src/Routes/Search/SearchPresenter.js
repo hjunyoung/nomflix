b5990673a5cc5b44fb83408e3c56b4fff9019992
Junyoung Hong <hjunyoung10@gmail.com>
2021-09-20 20:17:09 +0900

    🔨 Update presenters: Home,. TV, and Search

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Loader from 'Components/Loader';
+import Section from 'Components/Section';
+import Message from 'Components/Message';
+
+const Container = styled.div`
+  padding: 0 20px;
+`;
+
+const Form = styled.form`
+  margin-bottom: 50px;
+  width: 100%;
+`;
+
+const Input = styled.input`
+  all: unset;
+  font-size: 24px;
+  width: 100%;
+`;
 
 const SearchPresenter = ({
   movieResults,
@@ -9,8 +27,48 @@ const SearchPresenter = ({
   error,
   loading,
   handleSubmit,
+  handleChange,
 }) => {
-  return <h1>Search</h1>;
+  return (
+    <Container>
+      <Form onSubmit={handleSubmit}>
+        <Input
+          placeholder="Search Movies or TV Shows."
+          name="search"
+          value={searchTerm}
+          onChange={handleChange}
+        />
+      </Form>
+
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          {movieResults && movieResults.length > 0 && (
+            <Section title="Movie Results">
+              {movieResults.map((movie) => (
+                <span key={movie.id}>{movie.title}</span>
+              ))}
+            </Section>
+          )}
+          {tvResults && tvResults.length > 0 && (
+            <Section title="TV Show Results">
+              {tvResults.map((show) => (
+                <span key={show.id}>{show.name}</span>
+              ))}
+            </Section>
+          )}
+          {error && <Message color="#e74c3c" text={error} />}
+          {tvResults &&
+            movieResults &&
+            tvResults.length === 0 &&
+            movieResults.length === 0 && (
+              <Message text="Nothing found." color="#95a5a6" />
+            )}
+        </>
+      )}
+    </Container>
+  );
 };
 
 SearchPresenter.propTypes = {
@@ -20,6 +78,7 @@ SearchPresenter.propTypes = {
   error: PropTypes.string,
   loading: PropTypes.bool.isRequired,
   handleSubmit: PropTypes.func.isRequired,
+  handleChange: PropTypes.func.isRequired,
 };
 
 export default SearchPresenter;
