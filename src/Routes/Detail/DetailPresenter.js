9f4ea98de33109c275834823d5174f2b23f7d444
Junyoung Hong <hjunyoung10@gmail.com>
2021-09-21 02:21:07 +0900

    ✨ Add IMDb icon

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -53,6 +53,8 @@ const Title = styled.h3`
 
 const ItemContainer = styled.div`
   margin: 20px 0;
+  display: flex;
+  align-items: center;
 `;
 
 const Item = styled.span``;
@@ -65,6 +67,40 @@ const Overview = styled.p`
   opacity: 0.7;
   line-height: 1.4;
   width: 50%;
+`;
+
+const ImdbTooltip = styled.div`
+  opacity: 0;
+  position: absolute;
+  background-color: #262a2c;
+  font-size: 12px;
+  padding: 4px;
+  border: 1px solid rgba(20, 20, 20, 0.8);
+  border-radius: 3px;
+  width: 104px;
+  text-align: center;
+  left: 50%;
+  transform: translate(-50%, -170%);
+  pointer-events: none;
+  transition: opacity 0.05s linear;
+`;
+
+const Imdb = styled.img`
+  width: 100%;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+`;
+
+const ImdbLink = styled.a`
+  position: relative;
+  display: block;
+  width: 40px;
+  &:hover {
+    ${ImdbTooltip} {
+      opacity: 1;
+    }
+  }
 `;
 
 const DetailPresenter = ({ result, error, loading }) => {
@@ -121,6 +157,25 @@ const DetailPresenter = ({ result, error, loading }) => {
                 <Item>
                   {result.genres?.map((genre) => genre.name).join(' / ')}
                 </Item>
+                {result.imdb_id && (
+                  <>
+                    <Divider>•</Divider>
+                    <Item>
+                      <ImdbLink
+                        href={`https://www.imdb.com/title/${result.imdb_id}`}
+                        target="_blank"
+                      >
+                        <Imdb
+                          alt="imbd"
+                          src={require('../../assets/imdb.png').default}
+                        />
+                        <ImdbTooltip>
+                          <span>Open IMDb page</span>
+                        </ImdbTooltip>
+                      </ImdbLink>
+                    </Item>
+                  </>
+                )}
               </ItemContainer>
               <Overview>{result.overview || ''}</Overview>
             </Data>
